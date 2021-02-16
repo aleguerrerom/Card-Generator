9855fd9f677e02cf31fe28f7367d3f465d6f5462
Alejandro <Aleguemad94@yahoo.es>
2021-02-16 01:25:43 +0000

    FINAL

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
-/// SET A RAMDOM COLOR RED OR BLACK
-  let aleatorio = '';
-  const posibilities = ["red" , "black"];
-    aleatorio = Math.floor(Math.random() * posibilities.length);
-    let color = posibilities[aleatorio];
-//GENERADOR DE LETRAS Y SIMNOLOS ALEATORIO
-   let caracter    = '';
-   let values     = '23456789AQJK';
+//SET A RAMDOM COLOR RED OR BLACK
+// let aleatorio = '';
+// const posibilities = ["red" , "black"];
+// aleatorio = Math.floor(Math.random() * posibilities.length);
+// let color = posibilities[aleatorio];
+
+// GENERADOR DE LETRAS Y SIMBOLOS ALEATORIO
+   let caracter = '';
+   const values = '23456789AQJK';
    let valuesLength = values.length;
    caracter += values.charAt(Math.floor(Math.random() * valuesLength));
+  
 
-//GENERADOR DE 
-   let palo           = '';
-   let characters       = '♣♦♠♣';
+// GENERADOR DE SIMBOL
+   let palo             = '';
+   let characters       = '♣♦♠♥';
    let charactersLength = characters.length;
    palo += characters.charAt(Math.floor(Math.random() * charactersLength));
-    
-/// FIRST SYMBOL
-let SYMBOL1 = document.createElement("H2");//make an p element
-let node1 = document.createTextNode(palo);//create an innerhtml node
-SYMBOL1.className = "symbol-left";
-SYMBOL1.style.color = color;
-SYMBOL1.appendChild(node1);//add the text to the p element
+ 
+//CHOOSE COLOR  
+if (palo == "♥" ||  palo == "♦")
+    color = "red";
+    else color = "black"
 
-let element1 = document.getElementById("carta");//search the div1
-let child1 = document.getElementById("p1");//search the first p element
-element1.insertBefore(SYMBOL1, child1);
+///FIRST SYMBOL
+let symbol = document.createElement("H2");//make an p element
+let node = document.createTextNode(palo);//create an innerhtml node
+symbol.className = "symbol-left";
+
+symbol.style.color = color;
+symbol.appendChild(node);//add the text to the p element
+
+let element = document.getElementById("carta");//search the div1
+let child = document.getElementById("p1");//search the first p element
+element.insertBefore(symbol, child);
 
 //ADD LETTER OR NUMBER
-let SYMBOL2 = document.createElement("H2");//make an p element
+let symbol2 = document.createElement("H2");//make an p element
 let node2 = document.createTextNode(caracter);//create an innerhtml node
-SYMBOL2.className = "number";
-SYMBOL2.style.color = color;
-SYMBOL2.appendChild(node2);//add the text to the p element
+symbol2.className = "number";
+symbol2.style.color = color;
+symbol2.appendChild(node2);//add the text to the p element
 
 let element2 = document.getElementById("carta");//search the div1
 let child2 = document.getElementById("p1");//search the first p element
-element2.insertBefore(SYMBOL2, child2);
+element2.insertBefore(symbol2, child2);
 
 //ADD SECOND SYMBOL
 
-let SYMBOL3 = document.createElement("H2");//make an p element
+let symbol3 = document.createElement("H2");//make an p element
 let node3 = document.createTextNode(palo);//create an innerhtml node
-SYMBOL3.className = "symbol-right";
-SYMBOL3.style.color = color;
-SYMBOL3.appendChild(node3);//add the text to the p element
+symbol3.className = "symbol-right";
+symbol3.style.color = color;
+symbol3.appendChild(node3);//add the text to the p element
 
 let element3 = document.getElementById("carta");//search the div1
 let child3 = document.getElementById("p1");//search the first p element
-element3.insertBefore(SYMBOL3, child3);
+element3.insertBefore(symbol3, child3);
 
